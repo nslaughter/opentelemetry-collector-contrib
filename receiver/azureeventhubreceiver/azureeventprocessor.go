1bbbf223dfaf9bda30a9441f6e8ba6bc7d33bf99
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2024-05-20 16:54:58 -0500

    what eventprocessor does

--- receiver/azureeventhubreceiver/azureeventprocessor.go
+++ receiver/azureeventhubreceiver/azureeventprocessor.go
@@ -1,4 +1,7 @@
-package azureeventhubreceiver
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package azureeventhubreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/azureeventhubreceiver"
 
 // https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/messaging/azeventhubs/processor.go
 // https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/messaging/azeventhubs/processor_partition_client.go
@@ -19,111 +22,112 @@ package azureeventhubreceiver
 		--> put them into the entity thingy
 */
 
-// import (
-// 	"context"
-// 	"errors"
-// 	"fmt"
-// 	"time"
+/*
 
-// 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs"
-// 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs/checkpoints"
-// 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/container"
-// )
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
 
-// // Assuming there's a struct managing the processor setup
-// // type EventHubProcessor struct {
-// //     Processor *azeventhubs.Processor
-// // }
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs"
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs/checkpoints"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/container"
+)
 
-// // Updated initialization function using the new SDK components
-// func NewEventHubProcessor(ehConn, ehName, storageConn, storageCnt string) (*EventHubProcessor, error) {
-// 	checkpointingProcessor, err := newCheckpointingProcessor(ehConn, ehName, storageConn, storageCnt)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create checkpointing processor: %w", err)
-// 	}
+// Assuming there's a struct managing the processor setup
+type EventHubProcessor struct {
+    Processor *azeventhubs.Processor
+}
 
-// 	// Start processing events
-// 	return &EventHubProcessor{
-// 		Processor: checkpointingProcessor,
-// 	}, nil
-// }
+// Updated initialization function using the new SDK components
+func NewEventHubProcessor(ehConn, ehName, storageConn, storageCnt string) (*EventHubProcessor, error) {
+	checkpointingProcessor, err := newCheckpointingProcessor(ehConn, ehName, storageConn, storageCnt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create checkpointing processor: %w", err)
+	}
 
-// // Assume there's a function to start processing events
-// func (e *EventHubProcessor) StartProcessing(ctx context.Context) error {
-// 	// Start the processor
-// 	if err := e.Processor.Run(ctx); err != nil {
-// 		return fmt.Errorf("error running processor: %w", err)
-// 	}
-// 	return nil
-// }
+	// Start processing events
+	return &EventHubProcessor{
+		Processor: checkpointingProcessor,
+	}, nil
+}
 
-// // Assuming there's a struct managing the processor setup
-// type EventHubProcessor struct {
-// 	Processor *azeventhubs.Processor
-// }
+// Assume there's a function to start processing events
+func (e *EventHubProcessor) StartProcessing(ctx context.Context) error {
+	// Start the processor
+	if err := e.Processor.Run(ctx); err != nil {
+		return fmt.Errorf("error running processor: %w", err)
+	}
+	return nil
+}
 
-// // These are config values the processor factory can use to create processors:
-// //
-// //	(a) EventHubConnectionString
-// //	(b) EventHubName
-// //	(c) StorageConnectionString
-// //	(d) StorageContainerName
-// //
-// // You always need the EventHub variable values.
-// // And you need all 4 of these to checkpoint.
-// //
-// // I think the config values should be managed in the factory struct.
-// /*
-// func (pf *processorFactory) CreateProcessor() (*azeventhubs.Processor, error) {
-// 	// Create the consumer client
-// 	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(pf.EventHubConnectionString, pf.EventHubName, azeventhubs.DefaultConsumerGroup, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+// Assuming there's a struct managing the processor setup
+type EventHubProcessor struct {
+	Processor *azeventhubs.Processor
+}
 
-// 	// Create the blob container client for the checkpoint store
-// 	blobContainerClient, err := container.NewClientFromConnectionString(pf.StorageConnectionString, pf.StorageContainerName, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+// These are config values the processor factory can use to create processors:
+//
+//	(a) EventHubConnectionString
+//	(b) EventHubName
+//	(c) StorageConnectionString
+//	(d) StorageContainerName
+//
+// You always need the EventHub variable values.
+// And you need all 4 of these to checkpoint.
+//
+// I think the config values should be managed in the factory struct.
 
-// 	// Create the checkpoint store using the blob container client
-// 	checkpointStore, err := azeventhubs.NewBlobCheckpointStore(blobContainerClient, nil)
-// 	// checkpointStore, err := azeventhubs.NewBlobCheckpointStore(blobContainerClient, nil)
-// 	// if err != nil {
-// 	// 	return nil, err
-// 	// }
+func (pf *processorFactory) CreateProcessor() (*azeventhubs.Processor, error) {
+	// Create the consumer client
+	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(pf.EventHubConnectionString, pf.EventHubName, azeventhubs.DefaultConsumerGroup, nil)
+	if err != nil {
+		return nil, err
+	}
 
-// 	// Create the processor with checkpointing
-// 	processor, err := azeventhubs.NewProcessor(consumerClient, checkpointStore, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	// Create the blob container client for the checkpoint store
+	blobContainerClient, err := container.NewClientFromConnectionString(pf.StorageConnectionString, pf.StorageContainerName, nil)
+	if err != nil {
+		return nil, err
+	}
 
-// 	return processor, nil
-// }
-// */
+	// Create the checkpoint store using the blob container client
+	checkpointStore, err := azeventhubs.NewBlobCheckpointStore(blobContainerClient, nil)
+	checkpointStore, err := azeventhubs.NewBlobCheckpointStore(blobContainerClient, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the processor with checkpointing
+	processor, err := azeventhubs.NewProcessor(consumerClient, checkpointStore, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return processor, nil
+}
 
 // // checkpointing processor should be auth aware
 
-// func newCheckpointingProcessor(eventHubConnectionString, eventHubName, storageConnectionString, storageContainerName string) (*azeventhubs.Processor, error) {
-// 	blobContainerClient, err := container.NewClientFromConnectionString(storageConnectionString, storageContainerName, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	checkpointStore, err := checkpoints.NewBlobStore(blobContainerClient, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+func newCheckpointingProcessor(eventHubConnectionString, eventHubName, storageConnectionString, storageContainerName string) (*azeventhubs.Processor, error) {
+	blobContainerClient, err := container.NewClientFromConnectionString(storageConnectionString, storageContainerName, nil)
+	if err != nil {
+		return nil, err
+	}
+	checkpointStore, err := checkpoints.NewBlobStore(blobContainerClient, nil)
+	if err != nil {
+		return nil, err
+	}
 
-// 	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, azeventhubs.DefaultConsumerGroup, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	consumerClient, err := azeventhubs.NewConsumerClientFromConnectionString(eventHubConnectionString, eventHubName, azeventhubs.DefaultConsumerGroup, nil)
+	if err != nil {
+		return nil, err
+	}
 
-// 	return azeventhubs.NewProcessor(consumerClient, checkpointStore, nil)
-// }
-/*
+	return azeventhubs.NewProcessor(consumerClient, checkpointStore, nil)
+}
+
 func dispatchPartitionClients(processor *azeventhubs.Processor) {
 	for {
 		processorPartitionClient := processor.NextPartitionClient(context.TODO())
